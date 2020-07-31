3661cf56f3a2410912b02fb00678fdf0acc8403f
azizansari <ansariaziz001@gmail.com>
2020-07-31 18:30:33 +0530

    initial release version 1.0

---
+++ scripts/filter.js
@@ -0,0 +1,409 @@
+$(document).ready(function(){
+    $('.listing>li').click(function(){
+        var sel_item = $(this).html();
+    $('.box').hide();
+    $('.box').each(function(){
+        if($(this).hasClass(sel_item)){
+            $(this).animate({"width":"toggle"});
+            }
+    });
+    });
+    
+    
+});  
+
+/*for men section filter*/
+$(document).ready(function(){
+    $('.listing1>li').click(function(){
+        var sel_item = $(this).html();
+    $('.box1').hide();
+    $('.box1').each(function(){
+        if($(this).hasClass(sel_item)){
+            $(this).animate({"width":"toggle"});
+            }
+    });
+    });
+    
+    
+});
+/*For men Shirt subsection filter */
+$(document).ready(function(){
+    $('.listing2>li').click(function(){
+        var sel_item = $(this).html();
+    $('.box2').hide();
+    $('.box2').each(function(){
+        if($(this).hasClass(sel_item)){
+            $(this).animate({"width":"toggle"});
+            }
+    });
+    });
+    
+    
+});
+
+
+$(document).ready(function(){
+    $('.listing3>li').click(function(){
+        var sel_item = $(this).html();
+    $('.box2').hide();
+    $('.box2').each(function(){
+        if($(this).hasClass(sel_item)){
+            $(this).animate({"width":"toggle"});
+            }
+    });
+    });
+    
+    
+});
+
+
+
+$(document).ready(function(){
+    $('.listing4>li').click(function(){
+        var sel_item = $(this).html();
+    $('.box2').hide();
+    $('.box2').each(function(){
+        if($(this).hasClass(sel_item)){
+            $(this).animate({"width":"toggle"});
+            }
+    });
+    });
+    
+    
+});
+
+/*for men T-Shirt subsection Filter */
+$(document).ready(function(){
+    $('.listing5>li').click(function(){
+        var sel_item = $(this).html();
+    $('.box3').hide();
+    $('.box3').each(function(){
+        if($(this).hasClass(sel_item)){
+            $(this).animate({"width":"toggle"});
+            }
+    });
+    });
+    
+    
+});
+
+$(document).ready(function(){
+    $('.listing6>li').click(function(){
+        var sel_item = $(this).html();
+    $('.box3').hide();
+    $('.box3').each(function(){
+        if($(this).hasClass(sel_item)){
+            $(this).animate({"width":"toggle"});
+            }
+    });
+    });
+    
+    
+});
+
+$(document).ready(function(){
+    $('.listing7>li').click(function(){
+        var sel_item = $(this).html();
+    $('.box3').hide();
+    $('.box3').each(function(){
+        if($(this).hasClass(sel_item)){
+            $(this).animate({"width":"toggle"});
+            }
+    });
+    });
+    
+    
+});
+/*for men Jeans subsection Filter */
+$(document).ready(function(){
+    $('.listing8>li').click(function(){
+        var sel_item = $(this).html();
+    $('.box4').hide();
+    $('.box4').each(function(){
+        if($(this).hasClass(sel_item)){
+            $(this).animate({"width":"toggle"});
+            }
+    });
+    });
+    
+    
+});
+
+$(document).ready(function(){
+    $('.listing9>li').click(function(){
+        var sel_item = $(this).html();
+    $('.box4').hide();
+    $('.box4').each(function(){
+        if($(this).hasClass(sel_item)){
+            $(this).animate({"width":"toggle"});
+            }
+    });
+    });
+    
+    
+});
+
+$(document).ready(function(){
+    $('.listing10>li').click(function(){
+        var sel_item = $(this).html();
+    $('.box4').hide();
+    $('.box4').each(function(){
+        if($(this).hasClass(sel_item)){
+            $(this).animate({"width":"toggle"});
+            }
+    });
+    });
+    
+    
+});
+
+/*for Womwen section Filter */
+
+$(document).ready(function(){
+    $('.listing20>li').click(function(){
+        var sel_item = $(this).html();
+    $('.box20').hide();
+    $('.box20').each(function(){
+        if($(this).hasClass(sel_item)){
+            $(this).animate({"width":"toggle"});
+            }
+    });
+    });
+    
+    
+});
+
+/*for Womwen subsection Filter */
+$(document).ready(function(){
+    $('.listing21>li').click(function(){
+        var sel_item = $(this).html();
+    $('.box21').hide();
+    $('.box21').each(function(){
+        if($(this).hasClass(sel_item)){
+            $(this).animate({"width":"toggle"});
+            }
+    });
+    });
+    
+    
+});
+
+$(document).ready(function(){
+    $('.listing22>li').click(function(){
+        var sel_item = $(this).html();
+    $('.box21').hide();
+    $('.box21').each(function(){
+        if($(this).hasClass(sel_item)){
+            $(this).animate({"width":"toggle"});
+            }
+    });
+    });
+    
+    
+});
+
+$(document).ready(function(){
+    $('.listing23>li').click(function(){
+        var sel_item = $(this).html();
+    $('.box21').hide();
+    $('.box21').each(function(){
+        if($(this).hasClass(sel_item)){
+            $(this).animate({"width":"toggle"});
+            }
+    });
+    });
+    
+    
+});
+
+$(document).ready(function(){
+    $('.listing24>li').click(function(){
+        var sel_item = $(this).html();
+    $('.box22').hide();
+    $('.box22').each(function(){
+        if($(this).hasClass(sel_item)){
+            $(this).animate({"width":"toggle"});
+            }
+    });
+    });
+    
+    
+});
+
+
+$(document).ready(function(){
+    $('.listing25>li').click(function(){
+        var sel_item = $(this).html();
+    $('.box22').hide();
+    $('.box22').each(function(){
+        if($(this).hasClass(sel_item)){
+            $(this).animate({"width":"toggle"});
+            }
+    });
+    });
+    
+    
+});
+
+$(document).ready(function(){
+    $('.listing26>li').click(function(){
+        var sel_item = $(this).html();
+    $('.box22').hide();
+    $('.box22').each(function(){
+        if($(this).hasClass(sel_item)){
+            $(this).animate({"width":"toggle"});
+            }
+    });
+    });
+    
+    
+});
+
+
+$(document).ready(function(){
+    $('.listing27>li').click(function(){
+        var sel_item = $(this).html();
+    $('.box23').hide();
+    $('.box23').each(function(){
+        if($(this).hasClass(sel_item)){
+            $(this).animate({"width":"toggle"});
+            }
+    });
+    });
+    
+    
+});
+
+$(document).ready(function(){
+    $('.listing28>li').click(function(){
+        var sel_item = $(this).html();
+    $('.box23').hide();
+    $('.box23').each(function(){
+        if($(this).hasClass(sel_item)){
+            $(this).animate({"width":"toggle"});
+            }
+    });
+    });
+    
+    
+});
+
+$(document).ready(function(){
+    $('.listing29>li').click(function(){
+        var sel_item = $(this).html();
+    $('.box23').hide();
+    $('.box23').each(function(){
+        if($(this).hasClass(sel_item)){
+            $(this).animate({"width":"toggle"});
+            }
+    });
+    });
+    
+    
+});
+
+
+/*for Kids section Filter */
+
+$(document).ready(function(){
+    $('.listing30>li').click(function(){
+        var sel_item = $(this).html();
+    $('.box30').hide();
+    $('.box30').each(function(){
+        if($(this).hasClass(sel_item)){
+            $(this).animate({"width":"toggle"});
+            }
+    });
+    });
+    
+    
+});
+
+/*for Kids boys subsection section Filter */
+
+$(document).ready(function(){
+    $('.listing31>li').click(function(){
+        var sel_item = $(this).html();
+    $('.box31').hide();
+    $('.box31').each(function(){
+        if($(this).hasClass(sel_item)){
+            $(this).animate({"width":"toggle"});
+            }
+    });
+    });
+    
+    
+});
+
+$(document).ready(function(){
+    $('.listing32>li').click(function(){
+        var sel_item = $(this).html();
+    $('.box31').hide();
+    $('.box31').each(function(){
+        if($(this).hasClass(sel_item)){
+            $(this).animate({"width":"toggle"});
+            }
+    });
+    });
+    
+    
+});
+
+$(document).ready(function(){
+    $('.listing33>li').click(function(){
+        var sel_item = $(this).html();
+    $('.box31').hide();
+    $('.box31').each(function(){
+        if($(this).hasClass(sel_item)){
+            $(this).animate({"width":"toggle"});
+            }
+    });
+    });
+    
+    
+});
+
+
+/*for Kids Girls subsection section Filter */
+
+$(document).ready(function(){
+    $('.listing34>li').click(function(){
+        var sel_item = $(this).html();
+    $('.box32').hide();
+    $('.box32').each(function(){
+        if($(this).hasClass(sel_item)){
+            $(this).animate({"width":"toggle"});
+            }
+    });
+    });
+    
+    
+});
+
+$(document).ready(function(){
+    $('.listing35>li').click(function(){
+        var sel_item = $(this).html();
+    $('.box32').hide();
+    $('.box32').each(function(){
+        if($(this).hasClass(sel_item)){
+            $(this).animate({"width":"toggle"});
+            }
+    });
+    });
+    
+    
+});
+
+$(document).ready(function(){
+    $('.listing36>li').click(function(){
+        var sel_item = $(this).html();
+    $('.box32').hide();
+    $('.box32').each(function(){
+        if($(this).hasClass(sel_item)){
+            $(this).animate({"width":"toggle"});
+            }
+    });
+    });
+    
+    
+});
